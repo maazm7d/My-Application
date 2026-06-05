73df3b43ccda6fd65f56d26ceb20e98f5ca77fc2
M A A Z <maazm7d@gmail.com>
2026-06-05 16:30:31 +0530

    git: import application source code

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    namespace = "com.example.myapplication"
+    compileSdk = 35
+    
+    
+    defaultConfig {
+        applicationId = "com.example.myapplication"
+        minSdk = 36
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+        
+        vectorDrawables { 
+            useSupportLibrary = true
+        }
+        
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    
+    buildFeatures {
+        viewBinding = true
+        
+    }
+    
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.fromTarget("17"))
+    }
+}
+
+dependencies {
+
+
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+}
